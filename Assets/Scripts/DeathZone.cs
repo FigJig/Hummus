1d1667881a3c75f0a34783449c09ba5616e26872
FigJig <trent.mc.davies@gmail.com>
2020-02-01 18:00:07 +0000

    Added menu, ingame menu, deathzone and checkpoint, uimanager

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public CheckPoint checkPoint;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			StartCoroutine(DeathRoutine());	
+		}
+	}
+
+	IEnumerator DeathRoutine()
+	{
+		PlayerDataModule.Inst.transform.position = checkPoint.transform.position;
+
+		yield return null;
+	}
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+	public static UIManager Inst;
+
+	public static bool MenuIsActive;
+
+	public GameObject inGameMenu;
+	public TextMeshProUGUI chickpeaText;
+	public TextMeshProUGUI plantText;
+	public TextMeshProUGUI mineralText;
+	public TextMeshProUGUI summuhText;
+
+	private void Start()
+	{
+		if (Inst == null)
+		{
+			Inst = this;
+		}
+		else if (Inst != this)
+		{
+			Destroy(gameObject);
+		}
+
+		MenuIsActive = false;
+		inGameMenu.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("OpenMenu"))
+		{
+			inGameMenu.SetActive(!inGameMenu.activeInHierarchy);
+			MenuIsActive = inGameMenu.activeInHierarchy;
+			return;
+		}
+
+		if (inGameMenu.activeInHierarchy)
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				inGameMenu.SetActive(!inGameMenu.activeInHierarchy);
+				MenuIsActive = false;
+			}
+
+			if (Input.GetButtonDown("Cancel"))
+			{
+				Debug.Log("Pressed cancel");
+			}
+		}
+	}
+
+	public void UpdateChickpeaAmount(int amount)
+	{
+		chickpeaText.text = amount.ToString();
+	}
+
+	public void UpdatePlantAmount(int amount)
+	{
+		plantText.text = amount.ToString();
+	}
+
+	public void UpdateMineralAmount(int amount)
+	{
+		mineralText.text = amount.ToString();
+	}
+
+	public void UpdateSummuhAmount(int amount)
+	{
+		summuhText.text = amount.ToString();
+	}
+}
