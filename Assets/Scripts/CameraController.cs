fe90ccdfb9705cd2292f110bc891314bbc8a6a3c
FigJig <trent.mc.davies@gmail.com>
2020-02-01 10:52:08 +0000

    Added player movement, removed amplify examples, placeholder art

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Vector3 cameraOffsetFromPlayer;
+	public bool smoothFollow;
+	public float smoothFollowSpeed;
+
+	private Transform _player;
+
+    void Start()
+    {
+		_player = PlayerDataModule.Inst.transform;
+    }
+
+    void LateUpdate()
+    {
+		Vector3 playerPos = new Vector3(_player.position.x + cameraOffsetFromPlayer.x, _player.position.y + cameraOffsetFromPlayer.y, _player.position.z + cameraOffsetFromPlayer.z);
+		Vector3 targetPos;
+
+		if (smoothFollow)
+		{
+			targetPos = Vector3.Lerp(transform.position, playerPos, smoothFollowSpeed * Time.deltaTime);
+		}
+		else
+		{
+			targetPos = playerPos;
+		}
+
+		transform.position = targetPos;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+	private Animator _animator;
+	private PlayerDataModule _player;
+
+	private void Start()
+	{
+		_animator = GetComponent<Animator>();
+		_player = PlayerDataModule.Inst;
+	}
+
+	private void Update()
+	{
+		
+	}
+}
