6c231d9f6fc09e71c6a26f882df107324a36ff4d
FigJig <trent.mc.davies@gmail.com>
2020-02-01 14:39:52 +0000

    added loading multiple scenes

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+	public string[] scenesToLoad;
+
+   public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void LoadScenes()
+	{
+		SceneManager.LoadScene(scenesToLoad[0], LoadSceneMode.Single);
+
+		for (int i = 1; i < scenesToLoad.Length; i++)
+		{
+			SceneManager.LoadScene(scenesToLoad[i], LoadSceneMode.Additive);
+		}
+	}
+}
