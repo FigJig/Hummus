e70ba828014e1abaca3acf0f413b01a6d361c8a1
FigJig <trent.mc.davies@gmail.com>
2020-02-02 16:24:23 +0000

    hummus audio

---
+++ Assets/Scripts/HummusAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HummusAudio : MonoBehaviour
+{
+	private AudioSource _audioSource;
+	public AudioClip[] clips;
+	public float cooldown = 90f;
+	public int index;
+
+	private void Start()
+	{
+		_audioSource = GetComponent<AudioSource>();
+		cooldown = Random.Range(60f, 90f);
+		index = Random.Range(0, clips.Length);
+		StartCoroutine(PlayHummusCoroutine());
+	}
+
+	IEnumerator PlayHummusCoroutine()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(cooldown);
+
+			_audioSource.PlayOneShot(clips[index]);
+
+			cooldown = Random.Range(60f, 90f);
+			index = Random.Range(0, clips.Length);
+		}
+	}
+}
